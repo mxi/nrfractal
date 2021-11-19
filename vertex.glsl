68bf01c109bdd76d8595acb3ec3b8abd6195b5bf
Maxim Kasyanenko <maxim.kasyanenko@protonmail.ch>
2021-11-18 20:04:50 -0800

    Nicer assignment

--- vertex.glsl
+++ vertex.glsl
@@ -5,6 +5,5 @@ layout(location=0) in vec2 i_pos;
 out vec4 v_pos;
 
 void main() {
-    gl_Position = vec4(i_pos.xy, 0.0f, 1.0f);
-    v_pos = gl_Position;
+    gl_Position = v_pos = vec4(i_pos.xy, 0.0f, 1.0f);
 }
